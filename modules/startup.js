ef366800b26b13634e6a6859c2f24653c96a83aa
rzadmin <mtadmin@template1804.rac.hs-koblenz.de>
2019-05-09 17:29:14 +0200

    remote fix

--- modules/startup.js
+++ modules/startup.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
-
 const appId = process.env.APP_ID || 'parseServerId'
 const masterKey = process.env.MASTER_KEY || 'parseMasterKey'
 const serverURLBackend = process.env.SERVER_URL_BACKEND || '127.0.0.1'
 
 module.exports = {
   serverStartup: async request => {
+    console.log('cloud funciton started')
     if (!request.master) {
       throw 'not authorized'
     }
@@ -32,9 +32,11 @@ module.exports = {
       'X-Parse-Master-Key': masterKey
     }
     try {
+      console.log('get schema')
       let result = await axios.get(`${serverURLBackend}/schemas`, { headers })
       result.data.results.forEach(async element => {
         // if (element.className != '_User') {
+        console.log('working on: ', element.className)
         let clp = null
         if (element.className === '_User' || element.className === '_Role') clp = initialCLPUserRole
         else clp = initialCLP
